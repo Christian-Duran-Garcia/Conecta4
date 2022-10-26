4a1694d9c8ca8b15a0c87b3149d2569b738c58f2
Christian-Duran-Garcia <106398151+Christian-Duran-Garcia@users.noreply.github.com>
2022-10-26 10:35:18 -0500

    act5

---
+++ act5.py
@@ -0,0 +1,185 @@
+# Importar librerias necesarias
+from random import choice
+from turtle import *
+from freegames import floor, vector
+import sys
+
+# Declarar el estado inicial del juego
+state = {'score': 0}
+path = Turtle(visible=False)
+writer = Turtle(visible=False)
+aim = vector(5, 0)
+pacman = vector(-40, -80)
+ghosts = [
+    [vector(-180, 160), vector(10, 0)],
+    [vector(-180, -160), vector(0, 10)],
+    [vector(100, 160), vector(0, -10)],
+    [vector(100, -160), vector(-10, 0)],
+]
+
+# Formar la lista que forma la estructura lógica del tablero. 0 -> Pared, 1 -> Camino
+tiles = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
+
+# Definir la funcion que dibuja cuadros en el tablero
+def square(x, y):
+    # Dibujar los cuadros dando un camino en específico
+    path.up()
+    path.goto(x, y)
+    path.down()
+    path.begin_fill()
+
+    for count in range(4):
+        path.forward(20)
+        path.left(90)
+
+    path.end_fill()
+
+# Fucion para determinar un salto que existe entre los cuadros del tablero
+def offset(point):
+    x = (floor(point.x, 20) + 200) / 20
+    y = (180 - floor(point.y, 20)) / 20
+    index = int(x + y * 20)
+    return index
+
+# Funcion para determinar si la casilla en la que se encuentran los objetos es válida usando la función de offset
+def valid(point):
+    index = offset(point)
+
+    if tiles[index] == 0:
+        return False
+
+    index = offset(point + 19)
+
+    if tiles[index] == 0:
+        return False
+
+    return point.x % 20 == 0 or point.y % 20 == 0
+
+# Funcion que dibuja el tablero en la ventana
+def world():
+    bgcolor('blue')
+    path.color('black')
+
+    # Dibujar las bolitas (comida) del pacman
+    for index in range(len(tiles)):
+        tile = tiles[index]
+
+        if tile > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
+
+            if tile == 1:
+                path.up()
+                path.goto(x + 10, y + 10)
+                path.dot(2, 'white')
+
+ # Funcion para mover los fantasmas, cambiar de dirección a pacman, y determinar si se terminó el juego
+def move():
+    writer.undo()
+    writer.write(state['score'])
+
+    clear()
+
+    # Mover a pacman hacia la dirección que 
+    if valid(pacman + aim):
+        pacman.move(aim)
+
+    index = offset(pacman)
+
+    if tiles[index] == 1:
+        tiles[index] = 2
+        state['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+
+    # Determinar los posibles lugares que el pacman puede ir si se topa con una pared
+    for point, course in ghosts:
+        if valid(point + course):
+            point.move(course)
+        else:
+            options = [
+                vector(10, 0),
+                vector(-10, 0),
+                vector(0, 10),
+                vector(0, -10),
+            ]
+            plan = choice(options)
+            course.x = plan.x
+            course.y = plan.y
+
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(20, 'red')
+
+    update()
+
+    # Si los fantasmas tocan a pacman el juego se termina
+    for point, course in ghosts:
+        if abs(pacman - point) < 20:
+            sys.exit(3000)
+
+    # Modificar el tiempo para hacer que los fantasmas tomen la decisión más rápido
+    ontimer(move, 10)
+
+# Funcion para cambiar la dirección de pacman dado el input del teclado del usuario
+def change(x, y):
+    if valid(pacman + vector(x, y)):
+        aim.x = x
+        aim.y = y
+
+# Abrir una ventana de tamaño determinado
+setup(420, 420, 370, 0)
+
+# Ocultar la tortuga, el cursos, de la ventana
+hideturtle()
+
+# Cancelar la animación de la tortuga
+tracer(False)
+
+# Determinar el puntaje
+writer.goto(160, 160)
+writer.color('white')
+writer.write(state['score'])
+
+# Tomar el input del teclado del usuario
+listen()
+onkey(lambda: change(5, 0), 'Right')
+onkey(lambda: change(-5, 0), 'Left')
+onkey(lambda: change(0, 5), 'Up')
+onkey(lambda: change(0, -5), 'Down')
+
+# Dibujar el tablero
+world()
+
+# Movimimiento de los fantasmas, pacman, y verificar reglas del juego
+move()
+
+# Terminar el ciclo de la tortuga
+done()
