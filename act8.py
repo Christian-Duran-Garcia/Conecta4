0daa43a187b4dfa750f8cf0f736f6bde3427f083
Christian-Duran-Garcia <chriss.garcia29@gmail.com>
2022-10-27 11:01:55 -0500

    Correcion act8

--- act8.py
+++ act8.py
@@ -7,6 +7,8 @@ tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
 counter = 0
+colors = ['red', 'yellow', 'green', 'blue', 'cyan']
+rd_color = 'black'
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -33,48 +35,67 @@ def tap(x, y):
     spot = index(x, y)
     mark = state['mark']
     global counter
+    global rd_color
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
-        counter += 1
     else:
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
 
-def draw():
+    counter += 1
+    rd_color = sample(colors, 1)[0]
+
+def draw(rand_color):
     "Draw image and tiles."
     clear()
     goto(0, 0)
     shape(car)
     stamp()
     global counter
+    global rd_color
 
+    # Dibujar los cuadrados blancos en caso de que los tiles esten ocultos
     for count in range(64):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
 
+    # Asignar la marca del cuadro tiene una marca
     mark = state['mark']
 
+    # Dibujar los numeros de las tiles que estan ocultas
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
-        color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        color(rand_color)
+        if tiles[mark] < 10:
+            goto(x + 17, y) # Posicion centrada para numeros de un solo digito
+            write(tiles[mark], font=('Arial', 30, 'normal'))
+        else:
+            goto(x + 3, y) # Posicion centrada para numeros de dos digitos
+            write(tiles[mark], font=('Arial', 30, 'normal'))
+    
+    # Verificar si los tiles estan todos descubiertos
+    if not any(hide):
+       return
 
+    # Mostrar el contador de taps
+    up()
     goto(0, 200)
     color('black')
     write(counter, font=('Arial', 30, 'normal'))
+
+    # Actualizar la ventana
     update()
-    ontimer(draw, 100)
+    ontimer(draw(rd_color), 100)
 
 shuffle(tiles)
-setup(420, 420, 370, 0)
+setup(600, 600, 370, 0)
 addshape(car)
 hideturtle()
 tracer(False)
 onscreenclick(tap)
-draw()
+draw(rd_color)
 done()
