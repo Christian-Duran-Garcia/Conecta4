77090ef9c198edb6cab67c1c3da94c94aa12ab95
Christian-Duran-Garcia <chriss.garcia29@gmail.com>
2022-10-27 09:43:58 -0500

    Corrección de act7

--- act7.py
+++ act7.py
@@ -11,8 +11,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 400) / 5
+        speed.y = (y + 400) / 5
 
 def inside(xy):
     "Return True if xy within screen."
@@ -55,9 +55,9 @@ def move():
 
     draw()
 
-    for target in targets:
-        if not inside(target):
-            return
+    # for target in targets:
+    #    if not inside(target):
+    #        return
 
     ontimer(move, 50)
 
